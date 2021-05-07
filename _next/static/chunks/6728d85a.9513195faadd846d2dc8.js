da54becaae05d300ff6eabd0adc535046c845349
sbehrends <sbehrends@users.noreply.github.com>
2021-05-07 21:15:53 +0000

    deploy: 1084d8bc4210f33c9575628463f88c6337b27a82

---
+++ _next/static/chunks/6728d85a.9513195faadd846d2dc8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[3],{iNdW:function(t,n,i){"use strict";i.d(n,"a",(function(){return a}));var o=i("Lnxd");function a(t){return Object(o.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",stroke:"#000",strokeWidth:"2",d:"M12,22 L12,2 M2,12 L22,12"}}]})(t)}}}]);
